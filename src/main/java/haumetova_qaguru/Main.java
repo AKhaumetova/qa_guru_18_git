eaceb9eb2616deb6648e48bf80729f12ae0b41a9
AKhaumetova <Haumetovaa@mail.ru>
2024-07-17 00:28:11 +0300

    Update Main.java

--- src/main/java/haumetova_qaguru/Main.java
+++ src/main/java/haumetova_qaguru/Main.java
@@ -1,7 +1,8 @@
 package haumetova_qaguru;
 
 public class Main {
+     private static String msg = ""Hello qa guru!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"";
     public static void main(String[] args) {
-        System.out.println("Hello qa guru!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        System.out.println(msg);
     }
 }
