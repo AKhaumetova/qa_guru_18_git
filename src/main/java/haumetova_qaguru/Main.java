81db20ff3a1abe98bb3a2113db59c0edb85e5b16
1 <haumetovaa@mail.ru>
2024-09-29 21:05:48 +0300

    added getMessage() method test4

--- src/main/java/haumetova_qaguru/Main.java
+++ src/main/java/haumetova_qaguru/Main.java
@@ -2,6 +2,6 @@ package haumetova_qaguru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa guru 123");
+        System.out.println("Hello qa guru");
     }
 }
